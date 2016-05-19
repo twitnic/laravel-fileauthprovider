6666cf5af9dd4bb2329fadbe391bb94bc4887ac5
Dominic Richter <richter@pluspol.info>
2016-05-19 16:19:46 +0200

    added TravisCI Things ...

---
+++ tests/AuthenticationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class AuthenticationTest extends TestCase
+{
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+	public function testBasicExample()
+	{
+		echo __DIR__;
+	}
+}
